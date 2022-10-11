35ec6c9be8ec3b3524951869631ccc6e76a1af17
ksh <rkstkd98@naver.com>
2022-10-11 18:24:54 +0900

    make UI

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const button = document.querySelector('.buttonnn');
+console.log(button);
+button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
+    console.log('Click');
+});
+console.log('ss');
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const button = document.querySelector('.buttonnn');
+console.log(button);
+button?.addEventListener('click', () => {
+    console.log('Click');
+});
+console.log('ss');
